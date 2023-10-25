670b86c5f8c8c8439a34e4f8d7736e752288520c
Cristian Camilo Echeverri <crisechevrri@gmail.com>
2023-10-24 21:29:07 -0500

    feat(Book): Creating book functionality

--- src/js/books.js
+++ src/js/books.js
@@ -1,20 +1,66 @@
 document.addEventListener('DOMContentLoaded', async function () {
 
-    let response = await fetch('http://localhost:3000/books');
-    let data = await response.json();
-    showBooks(data);
+    const saveBookButton = document.getElementById('saveBookButton');
+    saveBookButton.addEventListener('click', function () {
+        const bookName = document.getElementById('name').value;
+        const bookAuthor = document.getElementById('author').value;
+        const bookGenre = document.getElementById('genre').value;
+        saveBook({ bookName, bookAuthor, bookGenre });
+    });
+
+    try {
+        let response = await fetch('http://localhost:3000/books');
+        let data = await response.json();
+        showBooks(data);
+    } catch (error) {
+        console.log(error);
+        showBooks(null);
+    }
 });
 
 function showBooks(books) {
     let arrayBooks = '';
-    books.forEach(book => {
-        arrayBooks += `<tr>
-            <td scope="row">${book.id}</td>
-            <td>${book.title}</td>
-            <td>${book.genre}</td>
-            <td>${book.author}</td>
+    if (!!books) {
+        books.forEach(book => {
+            arrayBooks += `<tr>
+                <td scope="row">${book.id}</td>
+                <td>${book.title}</td>
+                <td>${book.genre}</td>
+                <td>${book.author}</td>
+            </tr>`;
+        });
+    } else {
+        arrayBooks = `<tr class="table-warning">
+            <td colspan="4" class="text-center">No hay libros</td>
         </tr>`;
-    });
+    }
+
     const tableBody = document.getElementById('tableBody');
     tableBody.innerHTML = arrayBooks;
+}
+
+async function saveBook({ bookName, bookAuthor, bookGenre }) {
+    try {
+        let request = await fetch('http://localhost:3000/books', {
+            method: 'POST',
+            headers: {
+                "Content-Type": 'application/json'
+            },
+            body: JSON.stringify({
+                title: bookName,
+                author: bookAuthor,
+                genre: bookGenre
+            })
+        });
+
+        hideModal('createBook');
+    } catch (error) {
+
+    }
+}
+
+function hideModal(modalId) {
+    const existingModal = document.getElementById(modalId);
+    const modal = bootstrap.Modal.getInstance(existingModal);
+    modal.hide();
 }
